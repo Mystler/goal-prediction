9fe66ebacd14bf02c9e4a4e7c8377f3e51d0c6dc
Mystler <161936+Mystler@users.noreply.github.com>
2026-06-11 22:41:41 +0200

    feat: Ratings search

---
+++ src/routes/+page.svelte
@@ -0,0 +1,3 @@
+<svelte:head>
+  <title>Goal Projection</title>
+</svelte:head>
--- src/routes/ratings/+page.svelte
+++ src/routes/ratings/+page.svelte
@@ -1,18 +1,30 @@
 <script lang="ts">
   import { page } from "$app/state";
+
+  let search = $state("");
+
+  let ratings = $derived(Object.entries(page.data.ratings as Record<string, number>).toSorted((a, b) => b[1] - a[1]));
+  let filtered = $derived(ratings.filter((x) => x[0].toLowerCase().includes(search.toLowerCase())));
 </script>
 
 <svelte:head><title>ELO Ratings</title></svelte:head>
 
 <h2>ELO Ratings</h2>
 
+<div class="mb-4">
+  <label>
+    Search: <br />
+    <input type="text" bind:value={search} />
+  </label>
+</div>
+
 <table class="mx-auto">
   <tbody>
-    {#each Object.entries(page.data.ratings as Record<string, number>).toSorted((a, b) => b[1] - a[1]) as [nation, elo], index (nation)}
+    {#each filtered as entry (entry[0])}
       <tr class="odd:bg-slate-800">
-        <td class="text-right">{index + 1}</td>
-        <td class="text-left">{nation}</td>
-        <td class="text-right">{elo}</td>
+        <td class="text-right">{ratings.indexOf(entry) + 1}</td>
+        <td class="text-left">{entry[0]}</td>
+        <td class="text-right">{entry[1]}</td>
       </tr>
     {/each}
   </tbody>
